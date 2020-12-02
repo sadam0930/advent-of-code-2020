e512cb30340d5cc8076ee1631bd14e54d0bb80dd
Steven Adam <sadam@handy.com>
2020-12-02 18:46:48 -0500

    there are 2 problems

--- 12_1/solution.rb
+++ 12_1/solution.rb
@@ -1,17 +1,46 @@
-require 'set'
+require "set"
 
 TARGET = 2020
+INPUT_FILE = "input.txt"
 
-input = "input.txt"
-compliments = Set.new
+def two_sum
+  compliments = Set.new
 
-File.readlines(input).each do |line|
-  num = line.to_i
-  compliment = TARGET - num
-  if compliments.include?(compliment)
-    puts num * compliment
-    break
-  else
-    compliments.add(num)
+  input_array.each do |num|
+    compliment = TARGET - num
+    if compliments.include?(compliment)
+      puts num * compliment
+      break
+    else
+      compliments.add(num)
+    end
   end
 end
+
+def three_sum
+  input_array.each_with_index do |num, idx|
+    compliment_target = TARGET - num
+
+    sub_compliments = Set.new
+    input_array[idx+1, input_array.length].each do |sub|
+      sub_compliment = compliment_target - sub
+      if sub_compliments.include?(sub_compliment)
+        puts num * sub * sub_compliment
+        return
+      else
+        sub_compliments.add(sub)
+      end
+    end
+  end
+end
+
+def input_array
+  nums = []
+  File.readlines(INPUT_FILE).each do |line|
+    nums << line.to_i
+  end
+  nums
+end
+
+puts "solution 1: #{two_sum}"
+puts "solution 1: #{three_sum}"
